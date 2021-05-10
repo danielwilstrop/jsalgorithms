9e352af6b66f5f37cb0d61fc9be26ba13f562d28
Daniel Wilstrop <daniel.wilstrop@outlook.com>
2021-05-10 21:53:09 +0100

    added b/sort

--- bubbleSort.js
+++ bubbleSort.js
@@ -4,21 +4,18 @@ const swap = (arr, i1, i2) => {
   i1 = temp
 }
 
-const bubbleSort = (arr) => {
+const bubbleSort = (input) => {
   let swapping = true
 
   while (swapping) {
     swapping = false
-    for (let i = 0; i < arr.length - 1; i++) {
-      if (arr[i] > arr[i + 1]) {
-        swap(arr, i, i + 1)
+
+    for (let i = 0; i < input.length - 1; i++) {
+      if (input[i] > input[i + 1]) {
+        swap(input, i, i + 1)
         swapping = true
       }
     }
   }
-  return arr
+  return input
 }
-
-array = [3, 6, 7, 3, 2, 1, 1, 1]
-
-console.log(bubbleSort(array))
