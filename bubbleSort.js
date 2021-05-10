d5f195ca831f5b90df18b50cbff8abbf9bcb8551
Daniel Wilstrop <daniel.wilstrop@outlook.com>
2021-05-10 21:55:09 +0100

    reverse b/sort

--- bubbleSort.js
+++ bubbleSort.js
@@ -19,3 +19,20 @@ const bubbleSort = (input) => {
   }
   return input
 }
+
+const bubbleSort = (input) => {
+  let swapping = true
+
+  while (swapping) {
+    swapping = false
+
+    for (let i = 0; i < input.length - 1; i++) {
+      if (input[i] < input[i + 1]) {
+        swap(input, i, i + 1)
+        swapping = true
+      }
+    }
+  }
+
+  return input
+}
