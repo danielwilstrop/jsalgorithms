8307b460bceef1df17269e552a9ba900ce03f919
Daniel Wilstrop <daniel.wilstrop@outlook.com>
2021-05-24 21:24:47 +0100

    new

---
+++ fibonacci.js
@@ -0,0 +1,19 @@
+//sum of fib uo to n
+
+const memo = {}
+
+const fibonacci = (num) => {
+  let returnValue
+
+  if (memo[num]) {
+    returnValue = memo[num]
+  } else if (num === 0 || num === 1) {
+    returnValue = num
+  } else {
+    returnValue = fibonacci(num - 1) + fibonacci(num - 2)
+    memo[num] = returnValue
+  }
+  return returnValue
+}
+
+console.log(fibonacci(10))
